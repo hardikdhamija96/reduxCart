c7498828f4f15660d1e7a5729390a44d8d557b81
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-13 16:33:11 +0530

    setup router in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
