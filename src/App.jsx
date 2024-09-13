7526afddc6ccef5b98ba7d3aa378bb9ebfcba364
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-13 16:35:48 +0530

    added pages and route for home and cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Cart from "../pages/Cart";
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/cart" element={<Cart/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
