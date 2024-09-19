48deed3806208e939cfc945704e8ba3af23beea7
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-19 21:35:43 +0530

    added populated product page

---
+++ src/components/Products.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+const Products = () => {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      setProductList(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="flex flex-wrap justify-center gap-2">
+      {productList.map((product) => (
+        <div
+          key={product.id}
+          className="p-2  w-60 flex flex-col items-center gap-2 justify-between border-2 border-black rounded-md bg-white">
+          <h1 className="text-sm font-bold text-center">{product.title}</h1>
+          <img src={product.image} className="min-w-28" alt="" />
+          <div>
+            <h3 className="text-sm text-gray-800 font-bold">
+              {product.category}
+            </h3>
+            <h3 className="text-sm text-gray-800 font-bold">
+              Rs.{product.price}
+            </h3>
+            <button className="border-2 border-neutral-900 rounded-lg px-2 py-1">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
