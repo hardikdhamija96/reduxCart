d8127b8a0ed3c8337d18a60a374365855238ddb3
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-19 21:36:33 +0530

    update home page with product component and headings

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Products from "../src/components/Products";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="bg-blue-200 py-8 mx-auto">
+      <div>
+        <h3 className="font-bold text-center text-xl uppercase">
+          Demo project for Redux Toolkit in React
+        </h3>
+        <h2 className="font-bold text-center text-2xl uppercase text-blue-800">
+          Welcome to Redux Store!
+        </h2>
+      </div>
+      <section className="max-w-[90vw] mx-auto">
+        <h3 className="text-center font-bold uppercase text-xl">Product</h3>
+        <Products />
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
