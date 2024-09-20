4b86dd793a4024a68b2c8dce12d3cbf1f64a56e9
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-20 17:34:27 +0530

    basic cart page layout

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useSelector } from "react-redux"
 
 const Cart = () => {
+  const cartProducts = useSelector(state => state.cart)
+
+  useEffect(()=>{
+    console.log(cartProducts)
+  },[cartProducts])
+
   return (
-    <div>Cart</div>
+    <div className="p-2">
+      <h2 className="text-center text-2xl font-bold">Cart</h2>
+      <h2 className="text-right font-semibold pr-2">Total Items in Cart: {cartProducts.length}</h2>
+      {
+        cartProducts.map((product)=>(
+          <div key={product.id} className="bg-slate-500 m-1 p-2 flex justify-between text-white">
+            <h1>{product.title}</h1>
+            <h2>Rs.{product.price}</h2>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
