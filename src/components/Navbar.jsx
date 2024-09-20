11cccbb8f6c1364b4b7b748d8edae73f462ab757
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-20 17:20:56 +0530

    added cartItem counter functionality by subscribing to cart in store.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
 
 const Navbar = () => {
+  const cartItem = useSelector((state)=>state.cart);
+
+  // useEffect(()=>{
+  //   console.log(cartItem);
+  // },[cartItem])
   return (
     <div className='flex bg-blue-300 h-[10vh] justify-between px-8 items-center font-bold text-blue-950'>
         <span className='text-3xl'>Redux Cart</span>
@@ -8,7 +16,7 @@ const Navbar = () => {
             <Link to="/">Home</Link>
             <Link to="/cart">Cart</Link>
             <span>
-                Cart Item: 0
+                Cart Item: {cartItem.length}
             </span>
         </div>
     </div>
