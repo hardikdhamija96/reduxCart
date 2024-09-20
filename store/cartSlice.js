7cf3ef7c255bd297c8d652438e3bc07f30d1ba3c
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-20 11:59:17 +0530

    added cartSlice

---
+++ store/cartSlice.js
@@ -0,0 +1,27 @@
+const { createSlice } = require("@reduxjs/toolkit")
+
+const initialState = [];
+
+//createSlice need configuration object parameter
+const cartSlice = createSlice({
+    // name used in action types
+    name: "cart",
+    // initial state for reducer
+    initialState,
+    //reducers contain functions we need for our applications action type
+    reducers:{
+        addItem(state,action){
+            // Redux suggests to not mutate state directly
+            // but in toolkit createSlice take care of it
+            // createSlice automatically will run something like
+            // return [...state,action.payload]
+            state.push(action.payload);
+        },
+        removeItem(state,action){
+
+        },
+    }
+})
+
+export const {addItem,removeItem} = cartSlice.actions;
+export default cartSlice.reducer;
