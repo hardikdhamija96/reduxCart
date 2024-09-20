e5ac252bb2d246dabcb29fc6f1afef8a259acaa1
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-20 12:05:29 +0530

    wrapped app component with provider component from react redux and used our created store

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Cart from "../pages/Cart";
 import Navbar from "./components/Navbar";
+import {Provider} from "react-redux";
+import store from "../store/store";
 
 export default function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
